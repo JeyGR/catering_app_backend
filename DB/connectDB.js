4ccd10f94bae2f158e2ec2b3300b93b383035603
Jey <115620132+JeyGR@users.noreply.github.com>
2024-05-27 05:40:41 +0530

    Update connectDB.js

--- DB/connectDB.js
+++ DB/connectDB.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const { Client } = require("pg");
 
 const client = new Client({
-  host: "dpg-cndctpqcn0vc73f62dpg-a.oregon-postgres.render.com",
-  user: "library_da10_user",
-  port: "5432",
+  host: "monorail.proxy.rlwy.net",
+  user: "postgres",
+  port: "27999",
   password: process.env.pass,
   database: "catering",
   ssl: {
